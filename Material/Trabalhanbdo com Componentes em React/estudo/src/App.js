66e5942e7599fc6665c2cb262ff7ed84118003a4
cootoonhoo <cootoonhoo@gmail.com>
2022-02-12 18:28:23 -0300

    Conclusão do curso de Trabalhando com Componentes em React

---
+++ Material/Trabalhanbdo com Componentes em React/estudo/src/App.js
@@ -0,0 +1,21 @@
+import Item from "./components/Item";
+import Card from "./components/Card"
+
+const App = () =>{ 
+  return(
+    <>
+    <h1>Minha primeira apliocação com react!</h1>
+    <Item>
+      Texto 1
+    </Item>
+    <Item>
+      Texto 2
+    </Item>
+    <Item>
+      Texto 3
+    </Item>
+    <Card></Card>
+    </>
+  )
+}
+export default App  
